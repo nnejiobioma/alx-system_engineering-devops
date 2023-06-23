a29414c4d6ba702981d794a2549721b7d45bb680
nnejiobioma <nnejiobioma@gmail.com>
2023-06-23 15:34:50 +0000

    task updated again

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # This is a puppet code to crteate a file
- file { '/tmp/school':
-  ensure  => present,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
- }
+
+file { '/tmp/school':
+            ensure  => present,
+            path    => '/tmp/school',
+            mode    => '0744',
+            owner   => 'www-data',
+            group   => 'www-data',
+            content => 'I love Puppet',
+}
