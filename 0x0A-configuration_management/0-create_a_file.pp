f4594974e119463ceae65ef0a313274596d6ab7b
nnejiobioma <nnejiobioma@gmail.com>
2023-06-23 08:45:12 -0700

    added file to read me folder

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# This is a puppet code to crteate a file
-
+# manifest to create a file
 file { '/tmp/school':
-            ensure  => present,
-            path    => '/tmp/school',
-            mode    => '0744',
-            owner   => 'www-data',
-            group   => 'www-data',
-            content => 'I love Puppet',
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This is my task folder for 0x0A-configuration_management project
